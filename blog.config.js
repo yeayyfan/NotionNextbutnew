ad084f33a8472a872dce9a04cb9be24d96fac3a8
yeayyfan <157789046+yeayyfan@users.noreply.github.com>
2024-10-07 18:41:12 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -264,7 +264,7 @@ const BLOG = {
         process.env.NEXT_PUBLIC_COMMENT_UTTERRANCES_REPO || '', // 你的代码仓库名， 例如我是 'tangly1024/NotionNext'； 更多文档参考 https://utteranc.es/
 
   // giscus @see https://giscus.app/
-  /*
+  
   COMMENT_GISCUS_REPO: process.env.NEXT_PUBLIC_COMMENT_GISCUS_REPO || 'yeayyfan/Giscus', // 你的Github仓库名 e.g 'tangly1024/NotionNext'
   COMMENT_GISCUS_REPO_ID: process.env.NEXT_PUBLIC_COMMENT_GISCUS_REPO_ID || 'MDEwOlJlcG9zaXRvcnkzNTE5NTgwNTM=', // 你的Github Repo ID e.g ( 設定完 giscus 即可看到 )
   COMMENT_GISCUS_CATEGORY_ID:
@@ -289,7 +289,7 @@ const BLOG = {
   COMMENT_CUSDIS_SCRIPT_SRC:
         process.env.NEXT_PUBLIC_COMMENT_CUSDIS_SCRIPT_SRC ||
         '/js/cusdis.es.js', // change this if you're using self-hosted version
-        */
+        
 
   // gitalk评论插件 更多参考 https://gitalk.github.io/
   COMMENT_GITALK_REPO: process.env.NEXT_PUBLIC_COMMENT_GITALK_REPO || '', // 你的Github仓库名，例如 'NotionNext'
